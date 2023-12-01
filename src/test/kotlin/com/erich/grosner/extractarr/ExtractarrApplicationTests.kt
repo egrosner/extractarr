48885b04f93eb64bd1856b8c993e92aa52123d21
Erich Grosner <egrosner@gmail.com>
2023-11-30 23:12:37 -0500

    test properties file implemented

--- src/test/kotlin/com/erich/grosner/extractarr/ExtractarrApplicationTests.kt
+++ src/test/kotlin/com/erich/grosner/extractarr/ExtractarrApplicationTests.kt
@@ -4,9 +4,11 @@ import com.erich.grosner.extractarr.properties.FolderConfigProperties
 import org.junit.jupiter.api.Test
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
 @EnableConfigurationProperties(FolderConfigProperties::class)
+@TestPropertySource(properties = arrayOf("spring.profiles.active=test"))
 class ExtractarrApplicationTests {
 
 	@Test
