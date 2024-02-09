c89bc4979b55a08cd2e5bde39ab9157ac8b889df
Erich Grosner <egrosner@gmail.com>
2024-02-08 22:43:01 -0500

    fix: added a few new properties to the rest endpoint

--- src/main/kotlin/com/erich/grosner/extractarr/rarfiles/model/RarFilesResponse.kt
+++ src/main/kotlin/com/erich/grosner/extractarr/rarfiles/model/RarFilesResponse.kt
@@ -1,3 +1,3 @@
 package com.erich.grosner.extractarr.rarfiles.model
 
-data class RarFilesResponse(val fileName: String)
+data class RarFilesResponse(val fileName: String, val path: String, val extracted: Boolean)
